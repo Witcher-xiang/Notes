a7ff6dc28416bdc51f174a5461a668b622e60657
Witcher-xiang <1214334086@qq.com>
2020-08-05 20:35:43 +0800

    [Add]添加上下文

---
+++ 笔记记录呀/JS/promise.js
@@ -0,0 +1,24 @@
+//极简的实现
+class Promise {
+   
+    constructor(fn) {
+        this.callbacks = [];
+        fn(this._resolve.bind(this));
+    }
+    then(onFulfilled) {
+        this.callbacks.push(onFulfilled);
+    }
+    _resolve(value) {
+        this.callbacks.forEach(fn => fn(value));
+    }
+}
+
+//Promise应用
+let p = new Promise(resolve => {
+    setTimeout(() => {
+        console.log('done');
+        resolve('5秒');
+    });
+}).then((tip) => {
+    console.log(tip);
+})
