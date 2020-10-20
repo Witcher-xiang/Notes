ab26000b57f6e9137cfff96f91593428562895c5
wangyuxiang29 <wangyuxiang29@ejd.com>
2020-10-20 16:06:06 +0800

    [Add] 添加两数之和

---
+++ 笔记记录呀/数据结构与算法/数组/两数之和.js
@@ -0,0 +1,40 @@
+/**
+ * leetCode 第一题
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+ // 这是我第一个想到的方法 这种方法类似于暴力枚举 时间复杂度： O(n^2)
+var twoSum01 = function(nums, target) {
+    
+    for(let i = 0; i<nums.length; i++){
+        const item = nums[i];
+        const tempArr = [...nums]
+        tempArr.splice(i ,1)
+
+        const findIndex = tempArr.indexOf(target - item);
+        if(findIndex !== -1) return [i,findIndex +1];
+    }
+
+    return [];
+};
+
+// 第二种方法 用哈希表，一次遍历创建哈希表，用空间换时间
+var twoSum02 = function(nums, target) {
+    const hashMap = {};
+    for(let i=0; i< nums.length; i++){
+        const item = nums[i];
+        if(hashMap[item] !== undefined) return [hashMap[item],i]
+   
+        let key = target - item;
+        hashMap[key] = i;
+    }
+
+    return [];
+};
+
+
+let arr = [2,7,11,15];
+
+console.log(twoSum02(arr, 9))
