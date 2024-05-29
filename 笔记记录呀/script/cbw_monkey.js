76701062dfc66425674ab7d25db968a947e5a513
wangyuxiang29 <wangyuxiang04@corp.netease.com>
2024-05-29 10:40:00 +0800

    feat: 字母异位分组

--- 笔记记录呀/script/cbw_monkey.js
+++ 笔记记录呀/script/cbw_monkey.js
@@ -7,8 +7,26 @@
 // ==/UserScript==
 // @match        http://222.180.163.201:9111/HomePage.aspx?*
 
+// 多选款的ID
+const checkboxList = [
+  "chkTrainItem_1",
+  "chkTrainItem_2",
+  "chkTrainItem_3",
+  "chkTrainItem_4",
+  "chkTrainItem_5",
+  "chkTrainItem_6",
+  "chkTrainItem_7",
+];
 (function () {
-
+  const checkboxList = [
+    "chkTrainItem_1",
+    "chkTrainItem_2",
+    "chkTrainItem_3",
+    "chkTrainItem_4",
+    "chkTrainItem_5",
+    "chkTrainItem_6",
+    "chkTrainItem_7",
+  ];
   //*—————— 添加额外的库—————— */
   var script = document.createElement("script");
   var script01 = document.createElement("script");
@@ -29,7 +47,7 @@
   document.getElementsByTagName("head")[0].appendChild(script01);
 
   // 填充富文本
-  const fillRichText = (param) =>{
+  const fillRichText = (param) => {
     const iframe = document.getElementsByClassName("ke-edit-iframe")[0];
     // 获取iframe中的文档对象
     var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
@@ -37,33 +55,56 @@
     var element = iframeDoc.getElementsByClassName("ke-content")[0];
     element.innerText = param;
     console.log(iframe, element);
-  }
+  };
   // 填充Form表单
   async function fillForm(data) {
     const total = data.length;
-    console.log("让我看看csv转化的数据", data);
+    console.log("CSV读取的数据为：", data);
     for (let i = 0; i < total; i++) {
-      
       const currentData = data[i];
+      // 如果此行被填写过则跳过本行
+      if (isAlreadyRowDataFill(currentData)) {
+        continue;
+      }
+
       const form = document.forms[0];
-      
+
       for (const key in currentData) {
-        const input = form.elements.namedItem(key);
-        if (input) {
-          input.value = currentData[key];
+        if (checkboxList.includes(key)) {
+          const currentDom = form.elements.namedItem(key);
+          currentDom &&
+            (form.elements.namedItem(key).checked =
+              currentData[key] === "TRUE" || "true");
+        } else {
+          const input = form.elements.namedItem(key);
+          if (input) {
+            input.value = currentData[key];
+          }
+        }
+
+        if (key === "ke-content") {
+          fillRichText(currentData[key]);
         }
       }
-      // checkbox
-      form.elements.namedItem("chkTrainItem_7").checked = true;
-      fillRichText("富文本测试字段")
-      // var editor = document.querySelector("ke-content");
-      // var editor01 =  document.getElementsByClassName("ke-content")[0];
-      // console.log("editor:",editor,editor01,iframe)
-      // console.log("textAraea:",textAraea, textAraea.value)
 
-      // form.submit();
+      addRowDataToCache(currentData);
+      break;
     }
   }
+
+  // 判断缓存中的数据是否已经存在
+  const isAlreadyRowDataFill = (data) => {
+    const key = JSON.stringify(data);
+    if (localStorage.getItem(key) === key) return true;
+
+    return false;
+  };
+
+  // 将数据添加到缓存中
+  const addRowDataToCache = (data) => {
+    const key = JSON.stringify(data);
+    localStorage.setItem(key, key);
+  };
 
   // 读取 CSV 文件内容
   async function readCSVFile(file) {
@@ -75,8 +116,7 @@
         // 使用 jschardet 库检测文件编码
         const detectedEncoding = jschardet.detect(reader.result);
         const encoding = detectedEncoding.encoding;
-
-        console.log("encoding:", encoding, file);
+        
         // 如果文件编码不是 UTF-8，则使用 iconv-lite 库转换编码
         if (encoding && encoding.toUpperCase() !== "UTF-8") {
           const decodedContent = iconv.decode(reader.result, encoding);
@@ -113,11 +153,35 @@
     return data;
   }
 
-  const handleFileChange = async (file) => {
-    const text = await processCsvData(file);
+  const refreshInputDom = (input) =>{
+    // 上传后不清空文件
+    //   if (input.files && input.files[0]) {
+    //   // 获取input的父级节点
+    //   var parent = input.parentNode;
+      
+    //   // 创建一个新的input元素
+    //   var newInput = document.createElement("input");
+    //   newInput.type = "file";
+    //   newInput.onchange = function(file) { handleFileChange(this,file); };
+      
+    //   // 替换旧的input元素
+    //   parent.replaceChild(newInput, input);
+      
+    //   // 将新文件添加到新的input元素中
+    //   // newInput.files = input.files;
+    // }
+    // 上传后清空文件
+    if(input) input.value = "";
+  }
+  const handleFileChange = async (input,newFile) => {
+    
+  
+    console.log("执行了")
+    const text = await processCsvData(newFile);
     const formData = csvToData(text);
-    console.log(formData);
+
     fillForm(formData);
+    refreshInputDom(input);
   };
 
   function showFloatingTextBox() {
@@ -131,14 +195,6 @@
     textBox.style.zIndex = "9999";
     textBox.style.background = "white";
 
-    // 创建按钮元素
-    const btn = document.createElement("button");
-    btn.innerText = "确定";
-    btn.style.position = "fixed";
-    btn.style.top = "260px";
-    btn.style.right = "10px";
-    btn.style.zIndex = "9999";
-
     // 插入上传文件信息
     const form = document.createElement("form");
     form.setAttribute("enctype", "multipart/form-data");
@@ -148,42 +204,11 @@
     fileInput.setAttribute("name", "file");
     form.appendChild(fileInput);
 
-    const submitButton = document.createElement("button");
-    submitButton.setAttribute("type", "submit");
-    submitButton.innerText = "Upload";
-    form.appendChild(submitButton);
-
-    submitButton.onclick = (e) => {
-      e.preventDefault();
-      console.log("提交按钮");
-    };
-
-    fileInput.onchange = (file) => handleFileChange(file);
+    fileInput.onchange = function(file) { handleFileChange(this,file); };
 
     textBox.appendChild(form);
     // 将文本框和按钮添加到页面
     document.body.appendChild(textBox);
-    document.body.appendChild(btn);
-
-    // 点击按钮后返回输入的文本，并隐藏文本框
-    btn.addEventListener("click", () => {
-      const text = textBox.value;
-      textBox.style.right = "-310px";
-      setTimeout(() => {
-        textBox.remove();
-        btn.remove();
-      }, 300);
-      return text;
-    });
-
-    // 自动隐藏文本框
-    setTimeout(() => {
-      textBox.style.right = "-310px";
-      setTimeout(() => {
-        textBox.remove();
-        btn.remove();
-      }, 300);
-    }, 9990000);
   }
 
   // Main function
@@ -192,61 +217,5 @@
     showFloatingTextBox();
   }
 
-  main(); 
+  main();
 })();
-
-function readFile() {
-  return new Promise((resolve, reject) => {
-    let reader = new FileReader();
-    reader.onload = (e) => {
-      resolve(e);
-    };
-    reader.onerror = (e) => {
-      reject(e);
-    };
-    if (type === "text") {
-      reader.readAsText(file);
-    } else if (type === "binary") {
-      reader.readAsBinaryString(file);
-    } else {
-      reader.readAsArrayBuffer(file);
-    }
-  });
-}
-
-async function getData(file) {
-  let out = {};
-  let headers = {};
-  // 读取文件数据
-  let e = await readFile(file.raw, "text");
-  let data = e.target.result;
-  let buf = new Uint8Array(data);
-  // utf8格式的文件读取中文会乱码，添加BOM，使其格式变为utf-8 with bom
-  let blob = new Blob(["\ufeff" + data], { type: "text/csv; charset=utf-8" });
-  e = await readFile(blob);
-  data = e.target.result;
-  // 读取excel数据
-  let workbook = window.XLSX.read(data, {
-    type: "array",
-    raw: true,
-    codepage: 936,
-  });
-  // 读取所有sheet数据
-  workbook.SheetNames.forEach((n) => {
-    out[n] = window.XLSX.utils.sheet_to_json(workbook.Sheets[n], {
-      cellDates: true,
-      raw: true,
-    });
-    // 以横向方式读取数据，第一行为标题数组
-    headers[n] = window.XLSX.utils.sheet_to_json(workbook.Sheets[n], {
-      header: 1,
-      cellDates: true,
-      raw: true,
-    });
-  });
-  // 获取第一个sheet的数据
-  let sheetName = workbook.SheetNames[0];
-  arr = out[sheetName]; // 所有数据
-  header = header[sheetName];
-  console.log("workbook:", workbook);
-}
---
+++ 笔记记录呀/数据结构与算法/hot100/字母异位词分组.js
@@ -0,0 +1,21 @@
+//49. 字母异位词分组
+
+/*
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+*/
+
+const test = ["eat", "tea", "tan", "ate", "nat", "bat"];
+const fun = (str) => {
+    const map = new Map();
+    for (const item of str) {
+
+        const key = Array.from(item).sort().join('');
+        map.has(key) ? map.get(key).push(item) : map.set(key, [item]);
+    }
+
+    return Array.from(map.values()) 
+}
+
+// fun(test)
+console.log(fun(test));
